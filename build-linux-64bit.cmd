9f446d231f9d23d73c7fff66d2a5bd05c27647a9
MatusOllah <matusollah420@gmail.com>
2023-05-27 19:40:31 +0200

    add build scripts

---
+++ build-linux-64bit.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Unity\Hub\Editor\2021.3.25f1\Editor\Unity.exe" -quit -batchmode -logfile - ".\Logs\" -projectpath . -buildLinux64Player ".\Builds\linux-64bit\OrbWars.exe" -force-glcore
---
+++ build-windows-64bit.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\Unity\Hub\Editor\2021.3.25f1\Editor\Unity.exe" -quit -batchmode -logfile - ".\Logs\" -projectpath . -buildWindows64Player ".\Builds\windows-64bit\OrbWars.exe" -force-glcore
